878cb1117adff46217d68eafabb788957888fd01
Ben Staehle <92970185+benito2268@users.noreply.github.com>
2022-06-01 16:15:49 -0500

    Add files via upload

---
+++ downloads/start.ps1
@@ -0,0 +1,9 @@
+cd \plugins
+del /f plugin.jar
+cd -
+
+$client = new-object System.Net.WebClient
+$client.DownloadFile("http://benitobox.net/downloads/plugin.jar","C:\Users\Administrator\Downloads\Server\ConkerServer.exe\plugins")
+
+java -Xms14G -Xmx14G -XX:+UseG1GC -jar spigot-1.18.2.jar nogui
+PAUSE
